4a1b4a3033a88d0b508357b0a67936d194685f95
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-25 21:45:31 +0530

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
+import 'package:fooderlich/card3.dart';
+
+// 1
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    const Card1(),
+    const Card2(),
+    const Card3(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+// 2
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+// 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+// 6
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
+import 'package:fooderlich/home.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    // final darktheme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: Home(),
+    );
+  }
+}
